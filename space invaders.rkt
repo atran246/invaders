5aae38c9a73007a028e1d988a8b6d5b83d031108
Andrew Tran <atran246@gmail.com>
2013-02-07 12:58:24 -0600

    Added Unit Tests (Ex 134)

--- space invaders.rkt
+++ space invaders.rkt
@@ -2,6 +2,8 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |space invaders|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
 ; Andrew Tran Space Invaders
+(require 2htdp/image)
+(require 2htdp/universe)
 
 ; physical constants 
 (define HEIGHT 80)
@@ -13,10 +15,23 @@
 (define BACKGROUND (empty-scene WIDTH HEIGHT))
 (define SHOT (triangle 3 "solid" "red"))
 
+
+; A List-of-shots is one of: 
+; – empty
+; – (cons Shot List-of-shots)
+; interp.: the collection of shots fired and moving straight up
+
+; A Shot is a Number. 
+; interp.: the number represents the shot's y coordinate
+
+
 ; A ShotWorld is List-of-numbers. 
 ; interp.: the collection of shots fired and moving straight up
 ; ShotWorld -> ShotWorld 
 ; move each shot up by one pixel 
+(check-expect (tock (list 9 8 7)) (list 8 7 6)) 
+(check-expect (tock (list 100 46 12 9)) (list 99 45 11 8))
+(check-expect (tock empty) empty)
 (define (tock w)
   (cond
     [(empty? w) empty]
@@ -24,6 +39,8 @@
 
 ; ShotWorld KeyEvent -> ShotWorld 
 ; add a shot to the world if the space bar was hit 
+(check-expect (keyh (list 234 33) " ") (list 80 234 33))
+(check-expect (keyh (list 234 33) "A") (list 234 33))
 (define (keyh w ke)
   (cond
     [(key=? ke " ") (cons HEIGHT w)]
@@ -31,6 +48,10 @@
 
 ; ShotWorld -> Image 
 ; add each shot y on w at (MID,y) to the background image 
+(check-expect (to-image (list 234 33))
+                        (place-image SHOT XSHOTS 234 
+                                     (place-image SHOT XSHOTS 33 BACKGROUND)))
+(check-expect (to-image empty) BACKGROUND)
 (define (to-image w)
   (cond
     [(empty? w) BACKGROUND]
